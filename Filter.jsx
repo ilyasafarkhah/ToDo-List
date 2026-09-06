1f056cb7229e5d4c9e61ab2ab6add055a9fec5a0
ilyasafarkhah <ilyasafarkhah@gmail.com>
2026-09-06 11:54:12 +0330

    Update Filter.jsx

--- Filter.jsx
+++ Filter.jsx
@@ -3,7 +3,7 @@ function ToDoFilter(prop){
     return(
         <section className="filtertabs">
             {prop.tabs.map((tab)=>
-            <button className= {prop.activeTab===tab.value ? "btn selectedfilter":"btn"} key={tab.value} onClick={()=>prop.handleFilter(tab.value)} >{tab.value}</button>
+            <button className= {prop.activeTab===tab.value ? "txt btn selectedfilter":"txt btn"} key={tab.value} onClick={()=>prop.handleFilter(tab.value)} >{tab.value}</button>
             )}
         </section>        
     )
