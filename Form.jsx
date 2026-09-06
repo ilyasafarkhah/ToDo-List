eafdb6c6cd8930adedc737b2632e4e884fce871e
ilyasafarkhah <ilyasafarkhah@gmail.com>
2026-09-06 11:42:43 +0330

    Update Form.jsx
    Add more validation to prevent repeated tasks

--- Form.jsx
+++ Form.jsx
@@ -1,28 +1,38 @@
 function ToDoForm(prop){
 
-    function handleSubmit(e){
-        e.preventDefault()
-        if(prop.task.trim() === "" || prop.task.length > 100 ){
-            return
-        }
-        prop.handleSetList()
-        prop.setTask("")
-    }
+    const avalibleTasks = prop.taskList.map(task => task.text);
 
-    return(
-        <>
-        <form onSubmit={handleSubmit}>
-            <label className="maininput">
-                <input type="text" 
-                value={prop.task} 
-                onChange={prop.handleSetTask} 
-                placeholder="Add a new task..." />
-                <button type="submit">submit</button>            
-            </label>
-        </form>
-        
-        </>
-    )
+    function handleSubmit(e){
+        e.preventDefault()
+        if(prop.task.trim() === "" || prop.task.length > 100 ){
+            return
+        }
+
+        const isDuplicate = avalibleTasks.some(task =>
+            task.trim() === prop.task.trim()
+        )
+
+        if (isDuplicate){
+            return
+        }
+      
+        prop.handleSetList()
+        prop.setTask("")
+    }
+
+    return(
+        <>
+        <form onSubmit={handleSubmit}>
+            <label className="maininput">
+                <input type="text" 
+                value={prop.task} 
+                onChange={prop.handleSetTask} 
+                placeholder="Add a new task..." />
+                <button type="submit"><span className="txt" >Submit</span></button>            
+            </label>
+        </form>       
+        </>
+    )
 }
 
 export default ToDoForm
